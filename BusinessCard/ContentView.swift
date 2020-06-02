b4d6d354bd6ee7f26505af7224335575c344ac25
Liam Ratcliffe <liam.ratcliffe@btinternet.com>
2020-06-02 09:29:34 +0100

    Initial Commit

---
+++ BusinessCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BusinessCard
+//
+//  Created by Liam Ratcliffe on 02/06/2020.
+//  Copyright © 2020 Liam Ratcliffe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
